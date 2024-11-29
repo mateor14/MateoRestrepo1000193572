ccb23a0919c207f4c4b9066b66cfe778b7dac6c9
jonathansanchez2948 <jonathansanchez2948@correo.itm.edu.co>
2024-11-29 18:10:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     print("Hola mundo este es el primer cambio")
     print("segundo cambio")
     print("tercer cambio")
+    print("Excelente trabajo")
  
 if __name__ == "__main__":
     main()
