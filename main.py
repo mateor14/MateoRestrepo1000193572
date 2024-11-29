b67addba2947b8e3148a56e8d4ae7b21bda2aad8
Mateo Restrepo <97568461+mateor14@users.noreply.github.com>
2024-11-28 23:43:47 -0500

    tercer commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     print("Hola mundo este es el primer cambio")
-    print(" segundo cambio")
+    print("segundo cambio")
+    print("tercer cambio")
  
 if __name__ == "__main__":
     main()
